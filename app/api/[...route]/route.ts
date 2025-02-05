94e8bbeb464a869ef4cca9a5a5ad2c1933a7123d
Ananta <codeantu@gmail.com>
2025-02-05 13:32:39 +0530

    initialize database setup with users table and API endpoints

--- app/api/[...route]/route.ts
+++ app/api/[...route]/route.ts
@@ -1,19 +1,46 @@
-import { Hono } from 'hono'
-import { handle } from 'hono/vercel'
-import test from './test'
+import { Hono } from "hono";
+import { handle } from "hono/vercel";
+import test from "./test";
+import { drizzle } from "drizzle-orm/neon-http";
+import "dotenv/config";
+import { eq } from "drizzle-orm";
+import { usersTable } from "@/db/schema";
 
-export const runtime = 'nodejs'
+export const runtime = "nodejs";
+const db = drizzle(process.env.DATABASE_URL!);
 
-const app = new Hono().basePath('/api')
-app.route('/', test)
+const app = new Hono().basePath("/api");
+app.route("/", test);
 
-app.get('/hello', (c) => {
+app.get("/hello", (c) => {
   return c.json({
-    message: 'Hello from Hono!',
-  })
-})
+    message: "Hello from Hono!",
+  });
+});
 
+app.get("/add-user", (c) => {
+  return c.json({
+    message: "user",
+  });
+});
 
+app.post("/add-user", async (c) => {
+  const user: typeof usersTable.$inferInsert = {
+    name: "John",
+    age: 30,
+    email: "john@example.com",
+  };
 
-export const GET = handle(app)
-export const POST = handle(app)
+  await db.insert(usersTable).values(user);
+  console.log("New user created!");
+
+  const users = await db.select().from(usersTable);
+  console.log("Getting all users from the database: ", users);
+
+  return c.json({
+    message: "User created",
+  });
+});
+
+export const GET = handle(app);
+export const POST = handle(app);
