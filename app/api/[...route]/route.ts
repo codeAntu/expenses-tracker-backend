fbaefcbebec424ff06ceb965a6aa996f2a81aba7
Abinash Karmakar <codeAbinash@gmail.com>
2025-02-10 19:34:44 +0530

    Fix CROS error

--- app/api/[...route]/route.ts
+++ app/api/[...route]/route.ts
@@ -1,58 +1,19 @@
 import { Hono } from "hono";
-import { handle } from "hono/vercel";
-import test from "./test";
-import { drizzle } from "drizzle-orm/neon-http";
-import "dotenv/config";
-import { eq } from "drizzle-orm";
-import user from "./user";
 import { cors } from "hono/cors";
-import transaction from "./transaction";
-import auth from "./auth";
+import { handle } from "hono/vercel";
 
-export const runtime = "nodejs";
+export const runtime = "edge";
 
 const app = new Hono().basePath("/api");
+app.use("*", cors({ origin: "*" }));
 
-// app.use(
-//   "*",
-//   cors({
-//     origin: "*",
-//   })
-// );
-
-// app.route("/", test);
-// app.route("/", user);
-// app.route("/transactions", transaction);
-// app.route("/auth", auth);
-
-// export const GET = handle(app);
-// export const POST = handle(app);
-
-export const hono = new Hono().basePath("/api");
-export type HonoApp = typeof hono;
-
-const testRoute = hono.route("/", test);
-const userRoute = hono.route("/", user);
-const transactionRoute = hono.route("/transactions", transaction);
-const authRoute = hono.route("/auth", auth);
-
-const route = hono.get("/", async (c) => {
+const hello = app.get("/hello", (c) => {
   return c.json({
-    message: "Hello from hono",
-    status: true,
+    message: "Hello from Hono!",
   });
 });
 
-route.use(
-  "*",
-  cors({
-    origin: "*",
-  })
-);
-
-export type AppType =
-  | typeof route
-  | typeof testRoute
-  | typeof userRoute
-  | typeof transactionRoute
-  | typeof authRoute;
+export const GET = handle(app);
+export const POST = handle(app);
+export default app;
+export type AppType = typeof app | typeof hello;
